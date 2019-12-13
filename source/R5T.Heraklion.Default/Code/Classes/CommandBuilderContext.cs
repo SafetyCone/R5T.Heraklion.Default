43860da6bb689e40a2cc090852232991880da690
MinexAutomation <minex.automation@gmail.com>
2019-12-12 19:49:24 -0800

    Implemented CommandBuilderContext type.

---
+++ source/R5T.Heraklion.Default/Code/Classes/CommandBuilderContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.Piraeus;
+
+
+namespace R5T.Heraklion.Default
+{
+    public class CommandBuilderContext<TContext> : ICommandBuilderContext<TContext>
+    {
+        public ICommandBuilder CommandBuilder { get; }
+
+
+        public CommandBuilderContext(ICommandBuilder commandBuilder)
+        {
+            this.CommandBuilder = commandBuilder;
+        }
+
+        public ICommandBuilderContext<TNewContext> ChangeContext<TNewContext>()
+        {
+            var newContextCommandBuilderContext = new CommandBuilderContext<TNewContext>(this.CommandBuilder);
+            return newContextCommandBuilderContext;
+        }
+    }
+}
