7e21613700ae634759f4e0757af37acf63c6ef88
MinexAutomation <minex.automation@gmail.com>
2019-12-17 16:47:44 -0800

    Added command-builder extension allowing direct execution of command-builder context commands.

---
+++ source/R5T.Heraklion.Default/Code/Extensions/ICommandLineInvocationOperatorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+using R5T.Caledonia;
+using R5T.Heraklion.Extensions;
+
+
+namespace R5T.Heraklion.Default
+{
+    public static class ICommandLineInvocationOperatorExtensions
+    {
+        public static void Execute(this ICommandLineInvocationOperator commandLineOperator, string executableFilePath, ICommandBuilderContext commandBuilderContext, TextWriter writer)
+        {
+            var arguments = commandBuilderContext.BuildCommand();
+
+            var invocation = CommandLineInvocation.New(executableFilePath, arguments);
+
+            var result = commandLineOperator.Run(invocation);
+
+            if (result.ExitCode != 0)
+            {
+                throw new Exception($"Execution failed. Error:\n{result.GetErrorText()}\nOutput:\n{result.GetOutputText()}\nArguments:\n{arguments}");
+            }
+            else
+            {
+                writer.WriteLine(result.GetOutputText());
+                writer.WriteLine(result.GetErrorText());
+            }
+        }
+
+        public static void Execute(this ICommandLineInvocationOperator commandLineOperator, string executableFilePath, ICommandBuilderContext commandBuilderContext)
+        {
+            commandLineOperator.Execute(executableFilePath, commandBuilderContext, Console.Out);
+        }
+    }
+}
